167c37138da22bfc467489278d158db6f2c3af98
Matheus Pedrosa Mendes da Costa <matheuscosta-ieg@instituto.ad>
2023-10-09 13:36:30 -0300

    feat: fazendo tela do cvv

---
+++ app/src/main/java/com/acolhe/app/PaginaCvvActivity.java
@@ -0,0 +1,14 @@
+package com.acolhe.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.acolhe.acolhe_api.R;
+
+public class PaginaCvvActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.paginacvv);
+    }
+}
