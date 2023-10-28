d34ac87aa83be8e3f8877f356caefbe8e17aced1
RafalFDS2 <rafael.ferraz@picpay.com>
2023-10-27 23:16:16 -0300

    feat: Home Page logical + Update layout

--- app/src/main/java/com/acolhe/app/MainActivity.java
+++ app/src/main/java/com/acolhe/app/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        getBundles();
+//        getBundles();
         adicionarEventosClickCabecalho();
         adicionaEventosCLickRodape();
     }
@@ -57,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
         TextView saldoLayout = findViewById(R.id.valorSaldo);
         saldoLayout.setText(stringSaldo);
     }
-
     private void adicionarEventosClickCabecalho() {
         LinearLayout btnPerfil = findViewById(R.id.lnrLytVoltar);
         LinearLayout btnOfensiva = findViewById(R.id.lnrLytOfensiva);
@@ -104,8 +103,6 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.txtVwNomePagina);
         textView.setText(pageName);
     }
-
-
     private void adicionaEventosCLickRodape() {
         LinearLayout btnHome = findViewById(R.id.lnrLytHome);
         LinearLayout btnVideos = findViewById(R.id.lnrLytConteudo);
@@ -156,14 +153,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     private void abreFragmento(Class target){
+        FragmentManager fragmentManager = getSupportFragmentManager();
         if (target == Home.class) {
             findViewById(R.id.header).setVisibility(View.GONE);
+            Bundle extras = new Bundle();
+            extras.putString("teste", "testezinho");
+            Home home = new Home();
+            home.setArguments(extras);
+            fragmentManager.beginTransaction().replace(R.id.fragment, home).setReorderingAllowed(true).addToBackStack("name").commit();
         }else {
             findViewById(R.id.header).setVisibility(View.VISIBLE);
+            fragmentManager.beginTransaction().replace(R.id.fragment, target, null).setReorderingAllowed(true).addToBackStack("name").commit();
         }
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragment, target, null).setReorderingAllowed(true).addToBackStack("name").commit();
     }
 }
---
+++ app/src/main/java/com/acolhe/app/model/Satisfacao.java
@@ -0,0 +1,12 @@
+package com.acolhe.app.model;
+
+public enum Satisfacao {
+    MUITO_TRISTE("Muito triste"),
+    TRISTE("Triste"),
+    NORMAL("Normal"),
+    BEM("bem"),
+    MUITO_BEM("Muito bem");
+
+    Satisfacao(String prettyName) {
+    }
+}
