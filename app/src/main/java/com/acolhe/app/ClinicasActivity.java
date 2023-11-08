7876b89f4593d70e61b084d2dc34426fc4276cd7
RafaelFDS1 <rafael_fds@outlook.com>
2023-11-08 17:20:42 -0300

    refactor: Update Clinicas + Video Activity

--- app/src/main/java/com/acolhe/app/ClinicasActivity.java
+++ app/src/main/java/com/acolhe/app/ClinicasActivity.java
@@ -2,9 +2,11 @@ package com.acolhe.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
@@ -24,6 +26,8 @@ public class ClinicasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clinicas);
+        ImageView imageAcolhePlus = findViewById(R.id.imgVwAcolhePlus);
+        imageAcolhePlus.setOnClickListener(view -> startActivity(new Intent(ClinicasActivity.this, PaginaAcolhePlus.class)));
 
         getClinicas();
         iniciarLista();
